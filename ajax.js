79c64fff039e8fc8680e593de656b946d862c59c
msardissino <msardissino@yahoo.com.ar>
2018-11-27 17:35:48 -0300

    ajax5

--- ajax.js
+++ ajax.js
@@ -14,5 +14,33 @@ $(document).ready(function(){
     });
       
   });
+  $('.caja1').on('click','button.switchDifu', function(){
+    $.ajax('resultado2.html', {
+        beforeSend: function(){
+          $('.loader').text('Cargando...');
+        }
+    })
+    .done(function(respuesta){
+        $('.caja2').html(respuesta);
+      })
+    .always(function(){
+      $('.loader').text('Finalizo la Carga.');
+    });
+      
+  });
+  $('.caja1').on('click','button.switchFresh', function(){
+    $.ajax('resultado3.html', {
+        beforeSend: function(){
+          $('.loader').text('Cargando...');
+        }
+    })
+    .done(function(respuesta){
+        $('.caja2').html(respuesta);
+      })
+    .always(function(){
+      $('.loader').text('Finalizo la Carga.');
+    });
+      
+  });
 
 });
