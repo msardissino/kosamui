dc4cf2572ebd27142c4393e6bdf433582e31020e
msardissino <msardissino@yahoo.com.ar>
2018-11-27 15:28:17 -0300

    ajax

---
+++ ajax.js
@@ -0,0 +1,18 @@
+// alert("Funcion!");
+$(document).ready(function(){
+  $('.caja1').on('click','button.switchSpray', function(){
+    $.ajax('sprays.html', {
+        beforeSend: function(){
+          $('.loader').text('Cargando...');
+        }
+    })
+    .done(function(respuesta){
+        $('.caja2').html(respuesta);
+      })
+    .always(function(){
+      $('.loader').text('Finalizo la Carga.');
+    });
+      
+  });
+
+});
